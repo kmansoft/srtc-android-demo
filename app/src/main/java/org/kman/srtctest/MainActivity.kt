f97a40bacfaaa97823ca5b328940c0eb57493ffa
Kostya Vasilyev <kman@fastmail.com>
2024-12-24 11:03:29 -0800

    Initial commit

---
+++ app/src/main/java/org/kman/srtctest/MainActivity.kt
@@ -0,0 +1,58 @@
+package org.kman.srtctest
+
+import android.app.Activity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.main_activity)
+
+        mEditWhipServer = findViewById(R.id.whip_server)
+        mEditWhipToken = findViewById(R.id.whip_token)
+        mButtonConnect = findViewById(R.id.whip_connect)
+
+        mButtonConnect.setOnClickListener {
+            onClickConnect()
+        }
+    }
+
+    override fun onKeyDown(keyCode: Int, event: KeyEvent?): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_BACK && event != null && event.repeatCount ==  0) {
+            finish()
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
+    }
+ 
+    private fun onClickConnect() {
+        val server = mEditWhipServer.text.toString().trim()
+        if (server.isEmpty()) {
+            mEditWhipServer.error = getString(R.string.error_server_missing)
+            mEditWhipServer.requestFocus()
+            return
+        }
+        if (!server.startsWith("https://")) {
+            mEditWhipServer.error = getString(R.string.error_server_invalid)
+            mEditWhipServer.requestFocus()
+            return
+        }
+        mEditWhipServer.error = null
+
+        val token = mEditWhipToken.text.toString().trim()
+        if (token.isEmpty()) {
+            mEditWhipToken.error = getString(R.string.error_token_missing)
+            mEditWhipToken.requestFocus()
+            return
+        }
+        mEditWhipToken.error = null
+    }
+
+    private lateinit var mEditWhipServer: EditText
+    private lateinit var mEditWhipToken: EditText
+    private lateinit var mButtonConnect: Button
+}
