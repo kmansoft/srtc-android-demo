8825cd41401f17567e68b0840cc6b747c117165d
Kostya Vasilyev <kman@fastmail.com>
2024-12-30 17:36:53 -0800

    Camera preview

--- src/main/java/org/kman/srtctest/MainActivity.kt
+++ src/main/java/org/kman/srtctest/MainActivity.kt
@@ -4,15 +4,32 @@ import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.content.SharedPreferences
+import android.content.pm.PackageManager
+import android.graphics.Camera
+import android.graphics.SurfaceTexture
+import android.hardware.camera2.CameraCaptureSession
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraDevice
+import android.hardware.camera2.CameraManager
+import android.hardware.camera2.CaptureRequest
 import android.os.Bundle
+import android.os.Handler
+import android.os.HandlerThread
+import android.os.Looper
 import android.os.SystemClock
 import android.util.Base64
+import android.util.Range
 import android.view.KeyEvent
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.SurfaceView
 import android.widget.Button
 import android.widget.EditText
+import androidx.core.content.ContextCompat
 import androidx.preference.PreferenceManager
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.Request
+import okhttp3.RequestBody.Companion.create
 import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import org.json.JSONObject
@@ -20,7 +37,7 @@ import org.kman.srtctest.rtc.PeerConnection
 import org.kman.srtctest.util.MyLog
 import java.nio.charset.StandardCharsets
 
-class MainActivity : Activity() {
+class MainActivity : Activity(), SurfaceHolder.Callback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -31,10 +48,12 @@ class MainActivity : Activity() {
         mEditWhipServer = findViewById(R.id.whip_server)
         mEditWhipToken = findViewById(R.id.whip_token)
         mButtonConnect = findViewById(R.id.whip_connect)
+        mSurfaceViewPreview = findViewById(R.id.preview)
 
         mButtonConnect.setOnClickListener {
             onClickConnect()
         }
+        mSurfaceViewPreview.holder.addCallback(this)
 
         setFieldsFromPrefs()
         setFieldsFromIntent(intent)
@@ -43,8 +62,15 @@ class MainActivity : Activity() {
     override fun onDestroy() {
         super.onDestroy()
 
+        mSurfaceViewPreview.holder.removeCallback(this)
+
         mPeerConnection?.release()
         mPeerConnection = null
+
+        mCamera?.close()
+        mCamera = null
+
+        mMediaThread.quitSafely()
     }
 
     override fun onNewIntent(intent: Intent?) {
@@ -58,6 +84,63 @@ class MainActivity : Activity() {
             return true
         }
         return super.onKeyDown(keyCode, event)
+    }
+
+    override fun onPostResume() {
+        super.onPostResume()
+
+        if (!hasPermissions()) {
+            requestPermissions(arrayOf(PERM_CAMERA, PERM_RECORD_AUDIO), 1)
+        } else if (mPreviewSurface != null) {
+            initCameraCapture()
+        }
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        mPreviewSurface = holder.surface
+        if (hasPermissions()) {
+            if (mCamera == null) {
+                initCameraCapture()
+            } else {
+                updateCameraSession()
+            }
+        }
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        mPreviewSurface = null
+        updateCameraSession()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        for (result in grantResults) {
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                Util.toast(this, R.string.error_no_permissions)
+                return
+            }
+        }
+
+        if (mPreviewSurface != null) {
+            initCameraCapture()
+        }
+    }
+
+    private fun hasPermissions(): Boolean {
+        return ContextCompat.checkSelfPermission(
+            this,
+            android.Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.RECORD_AUDIO
+                ) == PackageManager.PERMISSION_GRANTED
     }
 
     private fun setFieldsFromPrefs() {
@@ -203,17 +286,156 @@ class MainActivity : Activity() {
         })
     }
 
+    private fun initCameraCapture() {
+        if (!mIsInitCameraDone) {
+            mIsInitCameraDone = true
+
+            // Find the camera
+            val cm = getSystemService(CameraManager::class.java)
+            val frontCameraId = cm.cameraIdList.find {
+                cm.getCameraCharacteristics(it).get(CameraCharacteristics.LENS_FACING) == CameraCharacteristics.LENS_FACING_FRONT
+            }
+
+            if (frontCameraId == null) {
+                Util.toast(this, R.string.error_no_front_camera)
+                return
+            }
+
+            initCameraCapture(frontCameraId)
+        }
+    }
+
+    private fun initCameraCapture(cameraId: String) {
+        val cm = getSystemService(CameraManager::class.java)
+        if (ContextCompat.checkSelfPermission(this, PERM_CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            cm.openCamera(cameraId, mCameraStateCallback, mMainHandler)
+        }
+    }
+
+    private fun onCameraOpened(camera: CameraDevice) {
+        if (isDestroyed) {
+            camera.close()
+            return
+        }
+
+        mCamera = camera
+        updateCameraSession()
+    }
+
+    private fun onCameraClosed(camera: CameraDevice) {
+        mCamera = null
+    }
+
+    private fun onCameraError(camera: CameraDevice, error: Int) {
+         Util.toast(this, R.string.error_camera_open)
+
+        mCamera?.close()
+        mCamera = null
+    }
+
+    private fun updateCameraSession() {
+        val camera = mCamera ?: return
+
+        mCameraSession?.close()
+        mCameraSession = null
+
+        val surfaceList = createCameraSurfaceList()
+        if (surfaceList.isNotEmpty()) {
+            @Suppress("DEPRECATION")
+            camera.createCaptureSession(surfaceList, mCameraSessionCallback, mMediaHandler)
+        }
+    }
+
+    private fun onCameraSessionConfigured(session: CameraCaptureSession) {
+        if (isDestroyed || mCamera == null) {
+            session.close()
+            return
+        }
+
+        val surfaceList = createCameraSurfaceList()
+        if (surfaceList.isEmpty()) {
+            session.close()
+            return
+        }
+
+        mCameraSession = session
+
+        val camera = requireNotNull(mCamera)
+        val request = camera.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW).apply {
+            for (surface in surfaceList) {
+                addTarget(surface)
+            }
+            set(CaptureRequest.CONTROL_AE_TARGET_FPS_RANGE, Range.create(15, 15))
+        }.build()
+
+        session.setRepeatingRequest(request, null, mMediaHandler)
+    }
+
+    private fun onCameraSessionConfigureFailed(session: CameraCaptureSession) {
+        Util.toast(this, R.string.error_camera_session_failed)
+    }
+
+    private fun createCameraSurfaceList(): List<Surface> {
+        val surfaceList = ArrayList<Surface>()
+        mPreviewSurface?.also {
+            surfaceList.add(it)
+        }
+        return surfaceList
+    }
+
+    private val mMainHandler = Handler(Looper.getMainLooper())
+    private val mMediaThread = HandlerThread("Media").apply { start() }
+    private val mMediaHandler = Handler(mMediaThread.looper)
+
     private lateinit var mSharedPrefs: SharedPreferences
 
     private lateinit var mEditWhipServer: EditText
     private lateinit var mEditWhipToken: EditText
     private lateinit var mButtonConnect: Button
+    private lateinit var mSurfaceViewPreview: SurfaceView
 
     private var mPeerConnection: PeerConnection? = null
+    private var mPreviewSurface: Surface? = null
+
+    private var mIsInitCameraDone = false
+    private var mCamera: CameraDevice? = null
+    private var mCameraSession: CameraCaptureSession? = null
+
+    private val mCameraStateCallback = object : CameraDevice.StateCallback() {
+        override fun onOpened(camera: CameraDevice) {
+            onCameraOpened(camera)
+        }
+
+        override fun onClosed(camera: CameraDevice) {
+            onCameraClosed(camera)
+        }
+
+        override fun onDisconnected(camera: CameraDevice) {
+            onCameraClosed(camera)
+        }
+
+        override fun onError(camera: CameraDevice, error: Int) {
+            onCameraError(camera, error);
+        }
+    }
+
+    private val mCameraSessionCallback = object : CameraCaptureSession.StateCallback() {
+        override fun onConfigured(session: CameraCaptureSession) {
+            onCameraSessionConfigured(session)
+        }
+
+        override fun onConfigureFailed(session: CameraCaptureSession) {
+            onCameraSessionConfigureFailed(session)
+        }
+    }
 
     companion object {
         private const val TAG = "MainActivity"
+
         private const val PREF_KEY_SERVER = "whip_server"
         private const val PREF_KEY_TOKEN = "whip_token"
+
+        private const val PERM_CAMERA = android.Manifest.permission.CAMERA
+        private const val PERM_RECORD_AUDIO =  android.Manifest.permission.RECORD_AUDIO
     }
 }
